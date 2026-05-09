a3f6d88ff31adc3d287d33b7072d305c26698a5d
EnginHoca <enginniyazihoca@gmail.com>
2026-05-09 12:46:32 +0300

    auth services added.

---
+++ CommerceHub.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using CommerceHub.Application.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceHub.Application.Interfaces
+{
+	public interface IAuthService
+	{
+
+		Task<AuthResponseDto> RegisterAsync(RegisterRequestDto registerRequestDto);
+		Task<AuthResponseDto> LoginAsync(LoginRequestDto loginRequestDto);
+	}
+}
